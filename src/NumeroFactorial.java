53b6835028a5fa2fac0ea6e2db6c20d86c4f2d6f
Santiago <jsvilaa@ufpso.edu.co>
2025-03-13 10:05:27 -0500

    Numero factorial

---
+++ src/NumeroFactorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumeroFactorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = Scanner.nextInt();
+
+        int factorial = 1;
+        int i = 1;
+        while (i <= numero) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+    }
+}
