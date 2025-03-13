4123cbe371e100e2318eadaaefbf95c4ab6ddb00
Santiago <jsvilaa@ufpso.edu.co>
2025-03-13 10:13:44 -0500

    con el numero ingresado muestra la tabla de multiplicar

---
+++ src/TablaDeMultiplicar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TablaDeMultiplicar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
