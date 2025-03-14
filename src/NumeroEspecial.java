197a8985f48585ecb88c4be7baa839b76588e387
Santiago <jsvilaa@ufpso.edu.co>
2025-03-14 07:39:43 -0500

    numero especial

---
+++ src/NumeroEspecial.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NumeroEspecial {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("ingrese un numero");
+        int num = input.nextInt();
+        System.out.println("el numero es:" + num);
+        if (num<=0) {
+            System.out.println("numero no valido");
+            input.close();
+            return;
+            
+            }
+            for (int k = 1; k <= num; k++) {
+               
+                for (int j = 1; j <= k; j++) {
+                    if (j % 2 == 1) {
+                        System.out.print(k);
+                    } else {
+                        System.out.print(k*-1);
+                    }    
+                    if (j<k) {
+                      System.out.print(" ");
+                            
+                    }
+                   
+
+                        
+                    
+                }
+                System.out.println();
+                    
+            } 
+
+        }
+    }
